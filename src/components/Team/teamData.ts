9cf142b79737c967a61e6062aa60d2a4a0292ca7
Annushka Agarwal <annushkaagarwal@dhcp-10-250-51-21.harvard.edu>
2025-02-23 15:51:46 -0500

    added personal data

---
+++ src/components/Team/teamData.ts
@@ -0,0 +1,23 @@
+export const teamData = {
+    // leads
+    "teamLeaders": [
+        {
+            "name" : "John Doe",
+            "role" : "Electrical Team Lead",
+            "image" : "/images/team/JohnDoe.jpg",
+            "bio" : "John is a passionate electrical engineer with a focus on power systems and control theory. He has a strong background in robotics and automation, and he is dedicated to developing innovative solutions that push the boundaries of technology."
+            
+        }
+    ],
+    // electrical
+    "Electrical": [
+        {
+            "name" : "John Doe",
+            "role" : "Electrical Team Lead",
+            "image" : "/images/team/JohnDoe.jpg",
+            "bio" : "John is a passionate electrical engineer with a focus on power systems and control theory. He has a strong background in robotics and automation, and he is dedicated to developing innovative solutions that push the boundaries of technology."
+            
+        }
+    ],
+
+}
